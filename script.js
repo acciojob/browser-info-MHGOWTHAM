787ebba122cbed55a4796df939ad0f10cb40d44f
GOWTHAM M H <117714418+MHGOWTHAM@users.noreply.github.com>
2023-06-19 22:58:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+var divEle=document.getElementById(browser-info);
+var bNmae=navigator.appName;
+var vName=navigator.appVersion;
+conaole.log(divEle.textContent="You are using "+bName+"version"+vName);
